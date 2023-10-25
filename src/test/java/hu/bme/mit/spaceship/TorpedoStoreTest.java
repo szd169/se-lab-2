550bdf1e5d8b92140de9ebe1b38e4a26bc00a749
Szilvágyi Dániel Zoltán <danaszilvagyi169@gmail.com>
2023-10-25 15:37:38 +0200

    unit tests added

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,4 +18,48 @@ class TorpedoStoreTest {
     // Assert
     assertEquals(true, result);
   }
+
+    @Test
+    void fire_Fail(){
+    TorpedoStore store = new TorpedoStore(1,1);
+    boolean result = store.fire(1);
+    assertEquals(false, result);
+    }
+
+    @Test
+    void isEmpty_Success(){
+    TorpedoStore store = new TorpedoStore(1);
+
+    store.fire(1);
+
+    assertEquals(true, store.isEmpty());
+
+    }
+
+    @Test
+    void torpCount(){
+    TorpedoStore store = new TorpedoStore(5);
+    store.fire(3);
+    store.fire(1);
+    assertEquals(1, store.getTorpedoCount());
+    }
+
+    @Test
+  void fire_Fail_Exception(){
+    TorpedoStore store = new TorpedoStore(5);
+
+    Exception e = assertThrowsExactly(IllegalArgumentException.class,()-> store.fire(6));
+
+    assertEquals("numberOfTorpedos", e.getMessage());
+  }
+
+   @Test
+    void notFail(){
+    TorpedoStore store = new TorpedoStore(5);
+
+    boolean suc = store.fire(5);
+
+    assertEquals(true, suc);
+  }
+
 }
